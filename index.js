6e80e9ccefda3ca4ff6b6fc441c774e7b88f7b85
yochirolee <yleecruz@gmail.com>
2024-02-13 12:49:19 -0500

    index2

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", (req, res) => {
-	res.send("Welcome to the API");
-});
+
 app.use("/api/genres", genres);
 app.use("/api/excel", excel);
 app.use("/api/tracking", require("./routes/tracking"));
+app.use("/", (req, res) => {
+	res.send("Welcome to the API");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
