1466e650c9ea6f8f886bd3379bb037f54980b335
yochirolee <yleecruz@gmail.com>
2024-02-13 21:25:46 -0500

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const genres = require("./routes/genres");
-const excel = require("./routes/excel");
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+const genres = require("./routes/genres");
+const excel = require("./routes/excel");
 
 const app = express();
 dotenv.config();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
